6f9482f3c83110ebeb7d3d90cbd149d38ab4241d
doomsterinc <doomsterinc1@gmail.com>
2015-10-29 18:19:42 -0200

    15

---
+++ www/js/routes.js
@@ -0,0 +1,34 @@
+angular
+  .module('whatsapp')
+  .config(config);
+ 
+function config ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+ 
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+ 
+    .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    });
+ 
+  $urlRouterProvider.otherwise('/tab/chats');
+}
