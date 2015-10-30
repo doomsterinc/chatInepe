353fbb624f141e636030e881bfadd585f65c20e0
doomsterinc <doomsterinc1@gmail.com>
2015-10-30 10:12:07 -0200

    6

--- www/js/app.js
+++ www/js/app.js
@@ -5,21 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('chatInepe', ['ionic', 'chatInepe.controllers', 'chatInepe.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
+angular
+  .module('chatInepe', [
+    'ionic',
+    'chatInepe.services'
+  ]);
